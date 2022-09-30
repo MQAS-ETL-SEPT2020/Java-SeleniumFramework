bdbabb238073bbe2635e63094a217b01db93c015
Pooja R <Pooja R@DESKTOP-UAO1U67>
2022-09-30 16:53:33 +0530

    Added script BooksPageTest.java

--- Java-Selenium-TestNG-Framework-master/src/test/java/tests/BooksPageTest.java
+++ Java-Selenium-TestNG-Framework-master/src/test/java/tests/BooksPageTest.java
@@ -1,30 +1,32 @@
 package tests;
 
-import org.testng.annotations.Test;
+	import org.testng.annotations.Test;
 
-import pages.BasePage;
-import pages.BooksPage;
-import pages.ClickonAddToCart;
-import pages.HomePage;
+	import pages.BasePage;
+	import pages.BooksPage;
+	import pages.ClickonAddToCart;
+	import pages.HomePage;
 
-public class BooksPageTest extends BasePage {
+	public class BooksPageTest extends BasePage {
 
-	@Test
-	public void AddbooksToCart() {
-		HomePage homePage = new HomePage(driver);
-		homePage.clickBooks();
-		// logger.log(LogStatus.INFO, "Clicking 'Electronics'");
+		@Test
+		public void AddbooksToCart() {
+			HomePage homePage = new HomePage(driver);
+			homePage.clickBooks();
+			// logger.log(LogStatus.INFO, "Clicking 'Electronics'");
 
-		BooksPage Books = new BooksPage(driver);
-		Books.computingInternet();
-		// logger.log(LogStatus.INFO, "Clicking on Books");
+			BooksPage Books = new BooksPage(driver);
+			Books.computingInternet();
+			// logger.log(LogStatus.INFO, "Clicking on Books");
 
-		ClickonAddToCart addToCart = new ClickonAddToCart(driver);
-		addToCart.AddToCart();
-		// logger.log(LogStatus.INFO, "Clicking on Computing and Internet");
+			ClickonAddToCart addToCart = new ClickonAddToCart(driver);
+			addToCart.AddToCart();
+			// logger.log(LogStatus.INFO, "Clicking on Computing and Internet");
 
-		HomePage homePage1 = new HomePage(driver);
-		homePage1.clickShoppingCartLink();
-		// logger.log(LogStatus.INFO, "Clicking on Shopping cart");
+			HomePage homePage1 = new HomePage(driver);
+			homePage1.clickShoppingCartLink();
+			// logger.log(LogStatus.INFO, "Clicking on Shopping cart");
+		}
 	}
-}
+
+
