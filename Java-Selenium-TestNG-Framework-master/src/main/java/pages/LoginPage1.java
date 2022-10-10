979d43435ed44164345558ecdd43b17e28143924
ASUS <ASUS@DESKTOP-EA3O7R6>
2022-10-10 15:28:33 +0530

    Pushing LoginPage to GitHub

--- Java-Selenium-TestNG-Framework-master/src/main/java/pages/LoginPage1.java
+++ Java-Selenium-TestNG-Framework-master/src/main/java/pages/LoginPage1.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage1 {
-
+    By login=By.xpath("//a[text()='Log in']");
 	By EmailTextField=By.id("Email");
 	By PasswordTextField=By.id("Password");
 	By RememberMeCheckBox=By.id("RememberMe");
@@ -21,14 +21,17 @@ public class LoginPage1 {
 		wait = new WebDriverWait(driver, 10);
 	}
 	
-	public void typeEmail(String EmailId) {
+	public void clickOnLogin() {
+		driver.findElement(login).click();
+	}
+	public void typeEmail() {
 		driver.findElement(EmailTextField).clear();
-		driver.findElement(EmailTextField).sendKeys(EmailId);
+		driver.findElement(EmailTextField).sendKeys("meghadevaraja1998@gmail.com");
 	}
 	
-	public void typePassword(String password) {
+	public void typePassword() {
 		driver.findElement(PasswordTextField).clear();
-		driver.findElement(PasswordTextField).sendKeys(password);
+		driver.findElement(PasswordTextField).sendKeys("Megha@123");
 	}
 	
 	public void clickRemembermeCheckbox() {
