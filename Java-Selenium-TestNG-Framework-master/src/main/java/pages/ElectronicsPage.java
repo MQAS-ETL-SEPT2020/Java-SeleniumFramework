882f7042fd2da0a94403acfa0d431a81f9cb4968
Anilkumarsm28 <anilkumarsm28@gmail.com>
2022-10-12 00:03:15 +0530

    updated ElectronicsPage.java

--- Java-Selenium-TestNG-Framework-master/src/main/java/pages/ElectronicsPage.java
+++ Java-Selenium-TestNG-Framework-master/src/main/java/pages/ElectronicsPage.java
@@ -2,27 +2,47 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ElectronicsPage {
-	
-	By Camera=By.xpath("//img[@alt='Picture for category Camera, photo']");
-	By Cellphones=By.xpath("//img[@alt='Picture for category Cell phones']");
-	
+
+	By Camera = By.xpath("//img[@alt='Picture for category Camera, photo']");
+	By Cellphones = By.xpath("//img[@alt='Picture for category Cell phones']");
+	By SmartPhones = By.xpath("(//div[@class=\"picture\"])[1]");
+	By SmartPhonePrice = By.xpath("//span[@itemprop=\"price\"]");
+	By AddtocartButton = By.id("add-to-cart-button-43");
+
 	WebDriver driver;
 	WebDriverWait wait;
-	
+
 	public ElectronicsPage(WebDriver driver) { // create a constructor and pass the driver instance
-		this.driver=driver; 
+		this.driver = driver;
 		wait = new WebDriverWait(driver, 10);
 	}
-	
+
 	public void clickCameraIcon() {
 		driver.findElement(Camera).click();
 	}
-	
-	public void clickCellPhonesIcon() {
-		driver.findElement(Cellphones).click();
+
+	public boolean clickCellPhonesIcon() {
+		return wait.until(ExpectedConditions.visibilityOfElementLocated(Cellphones)).isDisplayed();
+	}
+
+	public void clickSmartphoneLink() {
+		driver.findElement(By.xpath("(//div[@class=\"picture\"])[1]")).click();
+	}
+
+	public boolean verifySmartphoneQuantity() {
+		return wait.until(ExpectedConditions.visibilityOfElementLocated(SmartPhones)).isDisplayed();
+	}
+
+	public boolean verifySmartphonePrice() {
+		return wait.until(ExpectedConditions.visibilityOfElementLocated(SmartPhonePrice)).isDisplayed();
+	}
+
+	public void clickAddtoCartButton() {
+		driver.findElement(By.id("add-to-cart-button-43")).click();
 	}
 
 }
