99817a47fba242b9947328263f8a0ae373f9a9b0
ASUS <ASUS@DESKTOP-EA3O7R6>
2022-10-11 09:32:28 +0530

    Pushing HomePage

--- Java-Selenium-TestNG-Framework-master/src/main/java/pages/HomePage.java
+++ Java-Selenium-TestNG-Framework-master/src/main/java/pages/HomePage.java
@@ -9,8 +9,8 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class HomePage{
-	
+public class HomePage {
+
 	/*
 	 * By oculusLogo = By.cssSelector("._2v0_"); By signinLink =
 	 * By.xpath("//a[contains(text(), 'Log In or Sign Up')]"); By signinButton =
@@ -29,125 +29,123 @@ public class HomePage{
 	 * xpath("//div[@class='_2xvy _8yxy _2xvr _7ujs _8yxz']/span/a[contains(@data-testid, 'navlink-')]"
 	 * ); By forumsTab = By.cssSelector("#u_0_17");
 	 */
-	
-	By RegisterLink=By.xpath("//a[text()='Register']");
-	By LoginLink=By.xpath("//a[text()='Log in']");
-    By ShoppingCartLink=By.xpath("//a[text()='Shopping cart']");
-    By WishlistLink=By.xpath("//span[text()='Wishlist']");
-	By Books=By.xpath("(//a[@href='/books'])[1]");
-	By Computers=By.xpath("(//a[@href='/computers'])[1]");
-	By Electronics=By.xpath("(//a[@href='/electronics'])[1]");
-	By ApparelAndShoes=By.xpath("(//a[@href='/apparel-shoes'])[1]");
-	By DigitalDownloads=By.xpath("(//a[@href='/digital-downloads'])[1]");
-	By Jewelry=By.xpath("(//a[@href='/jewelry'])[1]");
-	By GiftCards=By.xpath("(//a[@href='/gift-cards'])[1]");
-	
+
+	By RegisterLink = By.xpath("//a[text()='Register']");
+	By LoginLink = By.xpath("//a[text()='Log in']");
+	By ShoppingCartLink = By.xpath("//a[text()='Shopping cart']");
+	By WishlistLink = By.xpath("//span[text()='Wishlist']");
+	By Books = By.xpath("(//a[@href='/books'])[1]");
+	By Computers = By.xpath("(//a[@href='/computers'])[1]");
+	By Electronics = By.xpath("(//a[@href='/electronics'])[1]");
+	By ApparelAndShoes = By.xpath("(//a[@href='/apparel-shoes'])[1]");
+	By DigitalDownloads = By.xpath("(//a[@href='/digital-downloads'])[1]");
+	By Jewelry = By.xpath("(//a[@href='/jewelry'])[1]");
+	By GiftCards = By.xpath("(//a[@href='/gift-cards'])[1]");
+
 	WebDriver driver;
 	WebDriverWait wait;
-	
+
 	public HomePage(WebDriver driver) {
-		this.driver=driver;
-		wait = new WebDriverWait(driver, 5); 
+		this.driver = driver;
+		wait = new WebDriverWait(driver, 5);
 	}
-	
+
 	public String getTitle() {
-		return driver.getTitle();	
+		return driver.getTitle();
 	}
-	
+
+	public void menu(Object menuname) {
+		if (menuname == DigitalDownloads) {
+			driver.findElement(DigitalDownloads).click();
+		} else {
+			driver.findElement(GiftCards).click();
+		}
+	}
+
 	public void clickRegisterLink() {
 		driver.findElement(RegisterLink).click();
 	}
-	
+
 	public void clickLoginLink() {
 		driver.findElement(LoginLink).click();
 	}
-	
+
 	public void clickShoppingCartLink() {
 		driver.findElement(ShoppingCartLink).click();
 	}
-	
+
 	public void clickWishlistLink() {
 		driver.findElement(WishlistLink);
-		}
-	
+	}
+
 	public void clickBooks() {
 		driver.findElement(Books).click();
-        }
-	
+	}
+
 	public void clickComputers() {
 		driver.findElement(Computers).click();
 	}
-	
+
 	public void clickElectronics() {
 		driver.findElement(Electronics).click();
 	}
-	
+
 	public void clickApparelAndShoes() {
 		driver.findElement(ApparelAndShoes).click();
 	}
-	
+
 	public void clickDigitalDownloads() {
 		driver.findElement(DigitalDownloads).click();
 	}
-	
+
 	public void clickJewelry() {
 		driver.findElement(Jewelry).click();
 	}
-	
+
 	public void clickGiftCards() {
 		driver.findElement(GiftCards).click();
 	}
-	
-/*	}
-	
-	public boolean getLogo() {
-		 return driver.findElement(oculusLogo).isDisplayed();
-	}
-	
-	public void clickLogInLink() {
-		 driver.findElement(signinLink).click();
-	}
-	
-	public void clickSigninLink() {
-		 wait.until(ExpectedConditions.elementToBeClickable(signinButton));
-		 driver.findElement(signinButton).click();
-	}
-	
-	public void hoverOverCommunityTab() {
-		new Actions(driver).moveToElement(driver.findElement(communityTab)).perform();
-	}
-	
-	public void clickOnForumsTab() {
-		driver.findElement(forumsTab).click();
-	}
-	
-	public void switchTabs() {
-		ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-		driver.switchTo().window(tabs.get(1));
-	}
-	
-	public void hoverOverHeadsetsTab() {
-		new Actions(driver).moveToElement(driver.findElement(headsetsTab)).perform();
-		
-	}
-	
-	public List<WebElement> headsetsDropdownResults(){
-		return driver.findElement(headsetsDropdown).findElements(headsetsDropdownResults);
-	}
 
-	public void clickOculusQuest() {
-		driver.findElement(oculusQuest).click();
-	}
-	
-	public void addToCart() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(buyNowButton));
-		driver.findElement(buyNowButton).click();
-		wait.until(ExpectedConditions.visibilityOfElementLocated(oneTwentyEightGB));
-		driver.findElement(oneTwentyEightGB).click();
-	}
-	
-	public boolean isCartPageLoaded() {
-		return wait.until(ExpectedConditions.visibilityOfElementLocated(checkoutButton)).isDisplayed();
-	}*/
-	
+	/*
+	 * }
+	 * 
+	 * public boolean getLogo() { return
+	 * driver.findElement(oculusLogo).isDisplayed(); }
+	 * 
+	 * public void clickLogInLink() { driver.findElement(signinLink).click(); }
+	 * 
+	 * public void clickSigninLink() {
+	 * wait.until(ExpectedConditions.elementToBeClickable(signinButton));
+	 * driver.findElement(signinButton).click(); }
+	 * 
+	 * public void hoverOverCommunityTab() { new
+	 * Actions(driver).moveToElement(driver.findElement(communityTab)).perform(); }
+	 * 
+	 * public void clickOnForumsTab() { driver.findElement(forumsTab).click(); }
+	 * 
+	 * public void switchTabs() { ArrayList<String> tabs = new
+	 * ArrayList<String>(driver.getWindowHandles());
+	 * driver.switchTo().window(tabs.get(1)); }
+	 * 
+	 * public void hoverOverHeadsetsTab() { new
+	 * Actions(driver).moveToElement(driver.findElement(headsetsTab)).perform();
+	 * 
+	 * }
+	 * 
+	 * public List<WebElement> headsetsDropdownResults(){ return
+	 * driver.findElement(headsetsDropdown).findElements(headsetsDropdownResults); }
+	 * 
+	 * public void clickOculusQuest() { driver.findElement(oculusQuest).click(); }
+	 * 
+	 * public void addToCart() {
+	 * wait.until(ExpectedConditions.visibilityOfElementLocated(buyNowButton));
+	 * driver.findElement(buyNowButton).click();
+	 * wait.until(ExpectedConditions.visibilityOfElementLocated(oneTwentyEightGB));
+	 * driver.findElement(oneTwentyEightGB).click(); }
+	 * 
+	 * public boolean isCartPageLoaded() { return
+	 * wait.until(ExpectedConditions.visibilityOfElementLocated(checkoutButton)).
+	 * isDisplayed(); }
+	 */
+
 }
