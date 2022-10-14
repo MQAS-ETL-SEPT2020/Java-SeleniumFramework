251eedb86396c37656acbecf9f27fb42d0a7faa8
Anilkumarsm28 <anilkumarsm28@gmail.com>
2022-10-14 16:34:24 +0530

    updated Checkoutpage.java

--- Java-Selenium-TestNG-Framework-master/src/main/java/pages/CheckoutPage.java
+++ Java-Selenium-TestNG-Framework-master/src/main/java/pages/CheckoutPage.java
@@ -1,5 +1,4 @@
 package pages;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,190 +8,195 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CheckoutPage {
  //Billing Address Field
-	By CheckoutPage = By.xpath("//h1[text()='Checkout']");
-	By BillingAddressField = By.xpath("//h2[text()='Billing address']");
-	By BillingAddress = By.id("billing-address-select");
-	By NewAddress = By.xpath("//option[text()='New Address']");
-	By FirstName = By.id("BillingNewAddress_FirstName");
-	By LastName = By.id("BillingNewAddress_LastName");
-	By Email = By.id("BillingNewAddress_Email");
-	By Company = By.id("BillingNewAddress_Company");
-	By CountryId = By.id("BillingNewAddress_CountryId");
-	By CountryName = By.xpath("//option[text()='India']");
-	By City = By.id("BillingNewAddress_City");
-	By Address1 = By.id("BillingNewAddress_Address1");
-	By ZipPostalCode = By.id("BillingNewAddress_ZipPostalCode");
-	By PhoneNumber = By.id("BillingNewAddress_PhoneNumber");
-	By Continue = By.xpath("//input[@onclick='Billing.save()']");
-
-	//Shipping Address feild
-	By ShippingAddress = By.xpath("//h2[text()='Shipping address']");
-	By ShippingAddressdropdown = By.id("shipping-address-select");
-	By ShippingNewAddress = By.xpath(("//option[text()='New Address'])[2]"));
-	// By ShippingFirstName = By.id("BillingNewAddress_FirstName");
-	// By LastName = By.id("BillingNewAddress_LastName");
-	// By Email = By.id("BillingNewAddress_Email");
-	By ShippingCompany = By.id("ShippingNewAddress_Company");
-	By ShippingCountryId = By.id("ShippingNewAddress_CountryId");
-	By ShippingCountryName = By.xpath("//option[text()='India']");
-	By ShippingCity = By.id("ShippingNewAddress_City");
-	By ShippingAddress1 = By.id("ShippingNewAddress_Address1");
-	By ShipppingZipPostalCode = By.id("ShippingNewAddress_ZipPostalCode");
-	By ShippingPhoneNumber = By.id("ShippingNewAddress_PhoneNumber");
-	By ShippingContinue = By.xpath("//input[@onclick='Shipping.save()']");
 	
-	//Payment Method
-	By CashonDelivery_RadioButton=By.xpath("Cash On Delivery (COD) (7.00)");
-	By CheckMoney_OrderRadioButton=By.xpath("//label[text()='Check / Money Order (5.00)']");
-	By Creditcard_RadioButton=By.xpath("//label[text()='Credit Card']");
-	By PurchaseOrder_RadioButton=By.xpath("//label[text()='Purchase Order']");
-	By Back_Button=By.xpath("(//a[text()='Back'])[1]");
+ By CheckoutPage = By.xpath("//h1[text()='Checkout']");
+ By BillingAddressField = By.xpath("//h2[text()='Billing address']");
+ By BillingAddress = By.id("billing-address-select");
+ By NewAddress = By.xpath("//option[text()='New Address']");
+ By FirstName = By.id("BillingNewAddress_FirstName");
+ By LastName = By.id("BillingNewAddress_LastName");
+ By Email = By.id("BillingNewAddress_Email");
+ By Company = By.id("BillingNewAddress_Company");
+ By CountryIdInBilling = By.id("BillingNewAddress_CountryId");
+ By CountryNameInBilling = By.xpath("//option[text()='India']");
+ By City = By.id("BillingNewAddress_City");
+ By Address1InBilling = By.id("BillingNewAddress_Address1");
+ By ZipPostalCodeInBilling = By.id("BillingNewAddress_ZipPostalCode");
+ By PhoneNumberInBilling = By.id("BillingNewAddress_PhoneNumber");
+ By ContinueInBilling = By.xpath("//input[@onclick='Billing.save()']");
+
+ //Shipping Address field
+	
+ By ShippingAddressField=By.xpath("(//div[@class='step-title'])[2]");
+ By ShippingAddress=By.id("shipping-address-select");
+ By NewShippingAddress=By.xpath("(//option[text()='New Address'])[2]");
+ By CountryIdInShipping=By.id("ShippingNewAddress_CountryId");
+ By CountryNameInShipping=By.xpath("(//option[text()='India'])[2]");
+ By CityInShipping=By.id("ShippingNewAddress_City");
+ By Address1InShipping=By.id("ShippingNewAddress_Address1");
+ By ZipPostalCodeInShipping=By.id("ShippingNewAddress_ZipPostalCode");
+ By PhoneNumberInShipping=By.xpath("//input[@name='ShippingNewAddress.PhoneNumber']");
+ By ContinueInShipping=By.xpath("(//input[@title='Continue'])[2]");
+	
+ //Payment Method
+	
+ By CashonDelivery_RadioButton=By.xpath("Cash On Delivery (COD) (7.00)");
+ By CheckMoney_OrderRadioButton=By.xpath("//label[text()='Check / Money Order (5.00)']");
+ By Creditcard_RadioButton=By.xpath("//label[text()='Credit Card']");
+ By PurchaseOrder_RadioButton=By.xpath("//label[text()='Purchase Order']");
+ By Back_Button=By.xpath("(//a[text()='Back'])[1]");
 	
 
-	WebDriver driver;
-	WebDriverWait wait;
+ WebDriver driver;
+ WebDriverWait wait;
 
-	public CheckoutPage(WebDriver driver) {
-		this.driver = driver;
-		wait = new WebDriverWait(driver, 10);
-	}
+ public CheckoutPage(WebDriver driver) {
+  this.driver = driver;
+  wait = new WebDriverWait(driver, 10);
+ }
 
-	public boolean isCheckOutPageSuccesful() {
-		return wait.until(ExpectedConditions.visibilityOfElementLocated(CheckoutPage)).isDisplayed();
-	}
+ public boolean isCheckOutPageSuccesful() {
+  return wait.until(ExpectedConditions.visibilityOfElementLocated(CheckoutPage)).isDisplayed();
+ }
 
-	public boolean isBillingAddressDisplayed() {
-		return wait.until(ExpectedConditions.visibilityOfElementLocated(BillingAddressField)).isDisplayed();
-	}
+ public boolean isBillingAddressDisplayed() {
+  return wait.until(ExpectedConditions.visibilityOfElementLocated(BillingAddressField)).isDisplayed();
+ }
 
-	public boolean isContinueButtonDisplayed() {
-		return wait.until(ExpectedConditions.visibilityOfElementLocated(Continue)).isDisplayed();
-	}
+ public boolean isContinueButtonDisplayed() {
+  return wait.until(ExpectedConditions.visibilityOfElementLocated(ContinueInBilling)).isDisplayed();
+ }
 
-	public void BillingDropdown() {
-		driver.findElement(BillingAddress).click();
-		WebElement drop1 = driver.findElement(BillingAddress);
-		Select sel1 = new Select(drop1);
-		sel1.selectByVisibleText("New Address");
-		driver.findElement(NewAddress).click();
-	}
+ public void BillingDropdown() {
+  driver.findElement(BillingAddress).click();
+  WebElement drop1 = driver.findElement(BillingAddress);
+  Select sel1 = new Select(drop1);
+  sel1.selectByVisibleText("New Address");
+  driver.findElement(NewAddress).click();
+ }
 
-	public boolean SelectNewAddress() {
-		return wait.until(ExpectedConditions.visibilityOfElementLocated(NewAddress)).isSelected();
-	}
+ public boolean SelectNewAddressInBilling() {
+  return wait.until(ExpectedConditions.visibilityOfElementLocated(NewAddress)).isSelected();
+ }
 
-	public void EnterFirstName(String Firstname) {
-		driver.findElement(FirstName).clear();
-		driver.findElement(FirstName).sendKeys(Firstname);
-	}
+ public void EnterFirstNameInBilling(String Firstname) {
+  driver.findElement(FirstName).clear();
+  driver.findElement(FirstName).sendKeys(Firstname);
+ }
 
-	public void EnterLastName(String Lastname) {
-		driver.findElement(LastName).clear();
-		driver.findElement(LastName).sendKeys(Lastname);
-	}
+ public void EnterLastNameInBilling(String Lastname) {
+  driver.findElement(LastName).clear();
+  driver.findElement(LastName).sendKeys(Lastname);
+ }
 
-	public void EnterEmailId(String Emailid) {
-		driver.findElement(Email).clear();
-		driver.findElement(Email).sendKeys(Emailid);
-	}
+ public void EnterEmailIdInBilling(String Emailid) {
+  driver.findElement(Email).clear();
+  driver.findElement(Email).sendKeys(Emailid);
+ }
 
-	public void EnterCompanyName(String Companyname) {
-		driver.findElement(Company).clear();
-		driver.findElement(Company).sendKeys(Companyname);
-	}
+ public void EnterCompanyNameInBilling(String Companyname) {
+  driver.findElement(Company).clear();
+  driver.findElement(Company).sendKeys(Companyname);
+ }
 
-	public void CountryDropdown() {
-		driver.findElement(CountryId).click();
-		WebElement drop = driver.findElement(CountryId);
-		Select sel = new Select(drop);
-		sel.selectByVisibleText("India");
-	}
+ public void CountryDropdownInBilling() {
+  driver.findElement(CountryIdInBilling).click();
+  WebElement drop = driver.findElement(CountryIdInBilling);
+  Select sel = new Select(drop);
+  sel.selectByVisibleText("India");
+ }
 
-	public boolean SelectCountry() {
-		return wait.until(ExpectedConditions.visibilityOfElementLocated(CountryName)).isSelected();
-	}
+ public boolean SelectCountryInBilling() {
+  return wait.until(ExpectedConditions.visibilityOfElementLocated(CountryNameInBilling)).isSelected();
+ }
 
-	public void EnterCityName(String Cityname) {
-		driver.findElement(City).clear();
-		driver.findElement(City).sendKeys(Cityname);
-	}
+ public void EnterCityNameInBilling(String Cityname) {
+  driver.findElement(City).clear();
+  driver.findElement(City).sendKeys(Cityname);
+ }
 
-	public void EnterAddress1(String Address) {
-		driver.findElement(Address1).clear();
-		driver.findElement(Address1).sendKeys(Address);
-	}
+ public void EnterAddress1InBilling(String Address) {
+  driver.findElement(Address1InBilling).clear();
+  driver.findElement(Address1InBilling).sendKeys(Address);
+ }
 
-	public void EnterZipCode(String ZipCode) {
-		driver.findElement(ZipPostalCode).clear();
-		driver.findElement(ZipPostalCode).sendKeys(ZipCode);
-	}
+ public void EnterZipCodeInBilling(String ZipCode) {
+  driver.findElement(ZipPostalCodeInBilling).clear();
+  driver.findElement(ZipPostalCodeInBilling).sendKeys(ZipCode);
+ }
 
-	public void EnterMobileNumber(String Mobilenumber) {
-		driver.findElement(PhoneNumber).clear();
-		driver.findElement(PhoneNumber).sendKeys(Mobilenumber);
-	}
+ public void EnterMobileNumberInBilling(String Mobilenumber) {
+  driver.findElement(PhoneNumberInBilling).clear();
+  driver.findElement(PhoneNumberInBilling).sendKeys(Mobilenumber);
+ }
 
-	public void ClickOnContinueInBillingAddress() {
-		driver.findElement(Continue).click();
+ public void ClickOnContinueInBillingAddress() {
+  driver.findElement(ContinueInBilling).click();
 
-	}
+ }
 
-	public boolean isShippingAddressDisplayed() {
-		return wait.until(ExpectedConditions.visibilityOfElementLocated(ShippingAddress)).isDisplayed();
+	
+	
+//<<<<<<<<<Shipping Address>>>>>>>>>>>>>>>>>
+	
+ public boolean ShippingAddressDisplayed() {
+  return wait.until(ExpectedConditions.visibilityOfElementLocated(ShippingAddressField)).isDisplayed();
+ }
 
-	}
+ public boolean isContinueButtonDisplayedInShipping() {
+  return wait.until(ExpectedConditions.visibilityOfElementLocated(ContinueInShipping)).isDisplayed();
+ }
+	
+ public void shippingDropdown() {
+  driver.findElement(ShippingAddress).click();
+  WebElement shippingAddress=driver.findElement(ShippingAddress);
+  Select newAddress=new Select(shippingAddress);
+  newAddress.selectByVisibleText("New Address");
+  //driver.findElement(NewAddress).click();
+ } 
+	
+  public void CountryDropDownInShippingAddress() {
+   driver.findElement(CountryIdInShipping).click();
+   WebElement country = driver.findElement(CountryIdInShipping);
+   Select selectCountry=new Select(country);
+   selectCountry.selectByVisibleText("India");
+   driver.findElement(CountryNameInShipping).click();
+  }
+  
+  public void EnterCityNameInShippingAddress(String Cityname) {
+   driver.findElement(CityInShipping).clear();
+   driver.findElement(CityInShipping).sendKeys(Cityname);
+  }
+  
+  public void EnterAddress1InShippingAddress(String Address) {
+   driver.findElement(Address1InShipping).clear();
+   driver.findElement(Address1InShipping).sendKeys(Address);
+  }
+  
+  public void EnterZipCodeInShippingAddress(String PortalCode) {
+   driver.findElement(ZipPostalCodeInShipping).clear();
+   driver.findElement(ZipPostalCodeInShipping).sendKeys(PortalCode);
+    }
 
-	public void shippingDropdown() {
-		driver.findElement(ShippingAddressdropdown).click();
-		WebElement drop3 = driver.findElement(ShippingNewAddress);
-		Select sel3 = new Select(drop3);
-		sel3.selectByVisibleText("New Address");
-		driver.findElement(NewAddress).click();
-	}
+  public void EnterMobileNumberInShippingAddress(String MobileNumber) {
+   driver.findElement(PhoneNumberInShipping).clear();
+   driver.findElement(PhoneNumberInShipping).sendKeys(MobileNumber);
+  }
 
-	public void shippingEnterCompanyName(String Companyname) {
-		driver.findElement(Company).clear();
-		driver.findElement(Company).sendKeys(Companyname);
-	}
+  public void ClickOnContinueInShippingAddress() {
+   driver.findElement(ContinueInShipping).click();
 
-	public void shippingCountryDropdown() {
-		driver.findElement(CountryId).click();
-		WebElement drop = driver.findElement(CountryId);
-		Select sel = new Select(drop);
-		sel.selectByVisibleText("India");
-	}
+  }
 
-	public boolean shippingSelectCountry() {
-		return wait.until(ExpectedConditions.visibilityOfElementLocated(CountryName)).isSelected();
-	}
-
-	public void shippingEnterCityName(String Cityname) {
-		driver.findElement(City).clear();
-		driver.findElement(City).sendKeys(Cityname);
-	}
-
-	public void shippingEnterAddress1(String Address) {
-		driver.findElement(Address1).clear();
-		driver.findElement(Address1).sendKeys(Address);
-	}
-
-	public void shippingEnterZipCode(String ZipCode) {
-		driver.findElement(ZipPostalCode).clear();
-		driver.findElement(ZipPostalCode).sendKeys(ZipCode);
-	}
-
-	public void shippingEnterMobileNumber(String Mobilenumber) {
-		driver.findElement(PhoneNumber).clear();
-		driver.findElement(PhoneNumber).sendKeys(Mobilenumber);
-	}
-
-	public boolean Radiobuttons_isdisplayed() {
-	 wait.until(ExpectedConditions.visibilityOfElementLocated(CashonDelivery_RadioButton)).isDisplayed();
-	wait.until(ExpectedConditions.visibilityOfElementLocated(CheckMoney_OrderRadioButton)).isDisplayed();
-		wait.until(ExpectedConditions.visibilityOfElementLocated(Creditcard_RadioButton)).isDisplayed();
-		  wait.until(ExpectedConditions.visibilityOfElementLocated(PurchaseOrder_RadioButton)).isDisplayed();
-		  return true;
-	}
+	
+  
+ public boolean Radiobuttons_isdisplayed() {
+  wait.until(ExpectedConditions.visibilityOfElementLocated(CashonDelivery_RadioButton)).isDisplayed();
+ wait.until(ExpectedConditions.visibilityOfElementLocated(CheckMoney_OrderRadioButton)).isDisplayed();
+  wait.until(ExpectedConditions.visibilityOfElementLocated(Creditcard_RadioButton)).isDisplayed();
+    wait.until(ExpectedConditions.visibilityOfElementLocated(PurchaseOrder_RadioButton)).isDisplayed();
+    return true;
+ }
 }
 // }
 
@@ -208,75 +212,75 @@ public class CheckoutPage {
 //
 //public class CheckoutPage {
 //
-//	By firstNameField = By.name("firstName");
-//	By lastNameField = By.name("lastName");
-//	By phoneField = By.name("phoneNumber");
-//	By addressField = By.name("address1");
-//	By cityField = By.name("city");
-//	By stateProvinceDropdown = By.cssSelector("._296s._47p_");
-//	By zipcodeField = By.name("postalCode");
-//	By emailField = By.name("email");
-//	By emailOptInCheckbox = By.name("optInAsGuest");
-//	By continueToPaymentButton = By.name("submit");
-//	By errorContainer = By.xpath("//div[@class=' _64f3'][contains(text(), 'Please specify a valid email')]");
+// By firstNameField = By.name("firstName");
+// By lastNameField = By.name("lastName");
+// By phoneField = By.name("phoneNumber");
+// By addressField = By.name("address1");
+// By cityField = By.name("city");
+// By stateProvinceDropdown = By.cssSelector("._296s._47p_");
+// By zipcodeField = By.name("postalCode");
+// By emailField = By.name("email");
+// By emailOptInCheckbox = By.name("optInAsGuest");
+// By continueToPaymentButton = By.name("submit");
+// By errorContainer = By.xpath("//div[@class=' _64f3'][contains(text(), 'Please specify a valid email')]");
 //
-//	WebDriver driver;
-//	WebDriverWait wait;
+// WebDriver driver;
+// WebDriverWait wait;
 //
-//	public CheckoutPage(WebDriver driver) {
-//		this.driver = driver;
-//		wait = new WebDriverWait(driver, 5);
-//	}
+// public CheckoutPage(WebDriver driver) {
+// this.driver = driver;
+// wait = new WebDriverWait(driver, 5);
+// }
 //
-//	public void enterFirstName(String firstName) {
-//		driver.findElement(firstNameField).clear();
-//		driver.findElement(firstNameField).sendKeys(firstName);
-//	}
+// public void enterFirstName(String firstName) {
+// driver.findElement(firstNameField).clear();
+// driver.findElement(firstNameField).sendKeys(firstName);
+// }
 //
-//	public void enterLastName(String lastName) {
-//		driver.findElement(lastNameField).clear();
-//		driver.findElement(lastNameField).sendKeys(lastName);
-//	}
+// public void enterLastName(String lastName) {
+// driver.findElement(lastNameField).clear();
+// driver.findElement(lastNameField).sendKeys(lastName);
+// }
 //
-//	public void enterPhoneNumber(String phone) {
-//		driver.findElement(phoneField).clear();
-//		driver.findElement(phoneField).sendKeys(phone);
-//	}
+// public void enterPhoneNumber(String phone) {
+// driver.findElement(phoneField).clear();
+// driver.findElement(phoneField).sendKeys(phone);
+// }
 //
-//	public void enterAddress(String address) {
-//		driver.findElement(addressField).clear();
-//		driver.findElement(addressField).sendKeys(address);
-//	}
+// public void enterAddress(String address) {
+// driver.findElement(addressField).clear();
+// driver.findElement(addressField).sendKeys(address);
+// }
 //
-//	public void enterCity(String city) {
-//		driver.findElement(cityField).clear();
-//		driver.findElement(cityField).sendKeys(city);
-//	}
+// public void enterCity(String city) {
+// driver.findElement(cityField).clear();
+// driver.findElement(cityField).sendKeys(city);
+// }
 //
-//	public void selectState(String state) {
-//		new Select(driver.findElement(stateProvinceDropdown)).selectByValue(state);
-//	}
+// public void selectState(String state) {
+// new Select(driver.findElement(stateProvinceDropdown)).selectByValue(state);
+// }
 //
-//	public void enterZipcode(String zipcode) {
-//		driver.findElement(zipcodeField).clear();
-//		driver.findElement(zipcodeField).sendKeys(zipcode);
-//	}
+// public void enterZipcode(String zipcode) {
+// driver.findElement(zipcodeField).clear();
+// driver.findElement(zipcodeField).sendKeys(zipcode);
+// }
 //
-//	public void enterEmail(String email) {
-//		driver.findElement(emailField).clear();
-//		driver.findElement(emailField).sendKeys(email);
-//	}
+// public void enterEmail(String email) {
+// driver.findElement(emailField).clear();
+// driver.findElement(emailField).sendKeys(email);
+// }
 //
-//	public void refresh() {
-//		driver.navigate().refresh();
-//	}
+// public void refresh() {
+// driver.navigate().refresh();
+// }
 //
-//	public boolean isErrorContainerDisplayed() {
-//		return wait.until(ExpectedConditions.visibilityOfElementLocated(errorContainer)).isDisplayed();
-//	}
+// public boolean isErrorContainerDisplayed() {
+// return wait.until(ExpectedConditions.visibilityOfElementLocated(errorContainer)).isDisplayed();
+// }
 //
-//	public boolean isContinueButtonEnabled() {
-//		return wait.until(ExpectedConditions.elementToBeClickable(continueToPaymentButton)).isDisplayed();
-//	}
+// public boolean isContinueButtonEnabled() {
+// return wait.until(ExpectedConditions.elementToBeClickable(continueToPaymentButton)).isDisplayed();
+// }
 //
 //}
